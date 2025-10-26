300dd8889dfc9b7117e7342c2bdf54bff92894c8
TanBinh <buitanbinh2005@gmail.com>
2025-10-26 13:23:28 +0700

    hoan thien trang home va responsive

---
+++ Page/home/assets/js/main.js
@@ -0,0 +1,174 @@
+const dataBirthday = [
+  {
+    id: 1,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-nong-nan-tinh-yeu.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 2,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-cam-chuong-quyen-ru.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 3,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-red-rose-real.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 4,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-luon-ben-em.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+];
+const dataOpen = [
+  {
+    id: 1,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-nong-nan-tinh-yeu.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 2,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-cam-chuong-quyen-ru.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 3,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-red-rose-real.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 4,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-luon-ben-em.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+];
+const dataTheme = [
+  {
+    id: 1,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-nong-nan-tinh-yeu.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 2,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-cam-chuong-quyen-ru.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 3,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-red-rose-real.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 4,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-luon-ben-em.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+];
+const dataDesign= [
+  {
+    id: 1,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-nong-nan-tinh-yeu.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 2,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-cam-chuong-quyen-ru.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 3,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-red-rose-real.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 4,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-luon-ben-em.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+];
+const dataFresh= [
+  {
+    id: 1,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-nong-nan-tinh-yeu.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 2,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-cam-chuong-quyen-ru.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 3,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-red-rose-real.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+  {
+    id: 4,
+    name: "Nồng Nàn Tình Yêu",
+    url: "https://flowercorner.b-cdn.net/image/cache/catalog/products/B%C3%B3%20Hoa/bo-hoa-hong-do-luon-ben-em.jpg.webp",
+    Price: "610,000VND",
+    
+  },
+];
+
+function renderProductList(data, selector) {
+  var html = data.map(function(product) {
+    return `
+      <div class="home-list-product-item">
+        <img src="${product.url}" alt="${product.name}" />
+        <a class="name" href="#">${product.name}</a>
+        <div class="price">
+          <span class="price-new">${product.Price}</span>
+        </div>
+        <a class="order-btn" href="#">Đặt hàng</a>
+      </div>
+    `;
+  }).join('');
+  $(selector).html(html);
+}
+
+$(document).ready(function() {
+  renderProductList(dataBirthday, '#product-list-birthday');
+  renderProductList(dataOpen, '#product-list-opening');
+  renderProductList(dataTheme, '#product-list-theme');
+  renderProductList(dataDesign, '#product-list-design');
+  renderProductList(dataFresh, '#product-list-fresh');
+});
