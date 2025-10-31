594126fec9560b9fbbbffac6d6992abc7229dcb4
Lê Văn Khởi <khoilv2859@ut.edu.vn>
2025-10-31 13:14:24 +0700

    chu de, hoa khai truong, hoa sinh nhat

---
+++ Page/home/includes/Menu.php
@@ -0,0 +1,102 @@
+<?php
+// Menu include - moved from home.php
+?>
+<div class="homeMenu">
+    <!-- CHỦ ĐỀ (moved to front, styled pink) -->
+    <div class="dropdown">
+        <button
+            class="btn dropdown-toggle text-pink fw-bold"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+            CHỦ ĐỀ
+        </button>
+        <ul class="dropdown-menu">
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/chu_de/hoa_chuc_mung.php">HOA CHÚC MỪNG</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/chu_de/hoa_cam_tay.php">HOA CẦM TAY</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/chu_de/hoa_tang_le.php">HOA TANG LỄ - HOA CHIA BUỒN</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- hoa sinh nhat -->
+    <div class="dropdown">
+        <button
+            class="btn dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+            HOA SINH NHẬT
+        </button>
+        <ul class="dropdown-menu">
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_sinh_nhat/hoa_sinh_nhat_sang_trong.php">HOA SINH NHẬT SANG TRỌNG</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_sinh_nhat/hoa_sinh_nhat_gia_re.php">HOA SINH NHẬT GIÁ RẺ</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_sinh_nhat/hoa_sinh_nhat_tang_nguoi_yeu.php">HOA SINH NHẬT TẶNG NGƯỜI YÊU</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- HOA KHAI TRƯƠNG (order updated, added giá rẻ page) -->
+    <div class="dropdown">
+        <button
+            class="btn dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+            HOA KHAI TRƯƠNG
+        </button>
+        <ul class="dropdown-menu">
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_khai_truong/hoa_khai_truong_de_ban.php">HOA KHAI TRƯƠNG ĐỂ BÀN</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_khai_truong/ke_hoa_khai_truong.php">KỆ HOA KHAI TRƯƠNG HIỆN ĐẠI</a>
+            </li>
+            <li>
+                <a class="dropdown-item ajax-link" href="/Page/home/hoa_khai_truong/hoa_khai_truong_gia_re.php">HOA KHAI TRƯƠNG GIÁ RẺ</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- THIẾT KẾ -->
+    <div class="dropdown">
+        <button
+            class="btn dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+            THIẾT KẾ
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">BÓA HOA</a></li>
+            <li><a class="dropdown-item" href="#">LẴNG HOA</a></li>
+            <li><a class="dropdown-item" href="#">GIỎ HOA</a></li>
+        </ul>
+    </div>
+
+    <!-- HOA TƯƠI -->
+    <div class="dropdown">
+        <button
+            class="btn dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false">
+            HOA TƯƠI
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">HOA HỒNG</a></li>
+            <li><a class="dropdown-item" href="#">HOA BABY</a></li>
+            <li><a class="dropdown-item" href="#">HOA HƯỚNG DƯƠNG</a></li>
+        </ul>
+    </div>
+</div>
---
+++ Page/home/includes/header.php
@@ -0,0 +1,28 @@
+<header>
+    <!-- icon social -->
+    <div class="homeIconSocial">
+        <i class="bi bi-facebook"></i>
+        <i class="bi bi-tiktok"></i>
+        <i class="bi bi-instagram"></i>
+    </div>
+    <!-- logo -->
+    <div class="homeLogo">
+        <!-- Use absolute paths so header works from pages in any subfolder -->
+        <a href="/Page/home/home.php"><img src="/img/Logo_LTW.jpg" alt="Logo" /></a>
+    </div>
+    <!-- search & login  -->
+    <div class="homeSearchLogin">
+        <div class="homeSearch">
+            <input type="text" name="Search" placeholder="Tìm kiếm" class="" />
+            <button>
+                <i class="bi bi-search"></i>
+            </button>
+        </div>
+        <div class="Home_cart">
+            <a href="#"><i class="bi bi-bag-fill"></i></a>
+        </div>
+        <div class="Home_user">
+            <a href="#"><i class="bi bi-person-fill"></i></a>
+        </div>
+    </div>
+</header>
